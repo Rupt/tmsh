0059814393cc95b36ff628323eb55c8f244018ca
Rupert Tombs <rupert.tombs@monumo.com>
2025-04-23 10:35:22 +0100

    Sketch module entities test

--- tests/test_model.py
+++ tests/test_model.py
@@ -43,3 +43,33 @@ class Model(unittest.TestCase):
         tmsh.model.setCurrent(two)
         self.assertEqual(tmsh.model.getCurrent(), two)
         tmsh.finalize()
+
+    def test_filename(self) -> None:
+        tmsh.initialize()
+        name = "abc"
+        tmsh.model.setFileName(name)
+        self.assertEqual(tmsh.model.getFileName(), name)
+        tmsh.finalize()
+
+    def test_entities(self) -> None:
+        tmsh.initialize()
+        self.assertEqual(tmsh.model.getEntities(), [])
+
+        point_a = tmsh.model.geo.addPoint(0.0, 0.0, 0.0)
+        point_b = tmsh.model.geo.addPoint(0.0, 0.0, 1.0)
+        point_c = tmsh.model.geo.addPoint(0.0, 1.0, 1.0)
+        line = tmsh.model.geo.addPolyline((point_a, point_b, point_c, point_a))
+        loop = tmsh.model.geo.addCurveLoop((line,))
+        print(line)
+        surface = tmsh.model.geo.addPlaneSurface((loop,))
+        print(surface)
+        tmsh.model.geo.synchronize()
+        volume = tmsh.model.geo.extrude(((surface, 2),), 1.0, 0.0, 0.0)
+        tmsh.model.geo.synchronize()
+        print(tmsh.model.getEntities())
+        print(tmsh.model.getEntities(0))
+        print(tmsh.model.getEntities(1))
+        print(tmsh.model.getEntities(2))
+        print(tmsh.model.getEntities(3))
+        print(volume)
+        tmsh.finalize()
