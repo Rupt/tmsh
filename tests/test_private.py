44242cc30dcc38a69994dfea8e74f82ba124a795
Rupert Tombs <rupert.tombs@monumo.com>
2025-04-25 11:55:17 +0100

    Test ostring

--- tests/test_private.py
+++ tests/test_private.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import ctypes
 import unittest
+
+import gmsh
 
 import tests
 import tmsh
@@ -18,3 +21,22 @@ class ErrorCode(unittest.TestCase):
         tmsh.logger.write(msg)
         with self.assertRaises(RuntimeError), tmsh._ErrorCode() as error_code:  # noqa: SLF001
             error_code.value = 1
+
+
+class Output(unittest.TestCase):
+    def test_ostring(self) -> None:
+        text = b"asdf\0"
+        ptr = ctypes.cast(
+            gmsh.lib.gmshMalloc(len(text)), ctypes.POINTER(ctypes.c_char)
+        )
+        ctypes.memmove(ptr, text, len(text))
+        assert ptr[: len(text)] == text
+        self.assertEqual(
+            tmsh._ostring(ctypes.cast(ptr, ctypes.c_char_p)),  # noqa: SLF001
+            text[:-1].decode(),
+        )
+
+    def test_ostring_error(self) -> None:
+        ptr = ctypes.c_char_p()
+        with self.assertRaisesRegex(RuntimeError, "null pointer"):
+            tmsh._ostring(ptr)  # noqa: SLF001
